b5b8c41e185c9ba23a487698555fd02e181d2056
Rez <probablyrez@gmail.com>
2024-04-28 12:55:35 +1000

    day2 part 2

--- cmd/d02/d02.go
+++ cmd/d02/d02.go
@@ -8,7 +8,7 @@ import (
 )
 
 func partOne(lines []string) {
-	diceBag := map[string]int{
+	bagDice := map[string]int{
 		"red":   12,
 		"green": 13,
 		"blue":  14,
@@ -16,35 +16,83 @@ func partOne(lines []string) {
 	sum := 0
 
 	for n, line := range lines {
-		gameDice := map[string]int {
-			"red": 0, 
-			"green": 0, 
-			"blue": 0,
-		}
+		
+		gameSuccess := true
 		game := line[strings.Index(line, ":")+2:]
 		sets := strings.Split(string(game), "; ")
 
 		// fmt.Println(sets)
 		for _, set := range sets {
+			gameDice := map[string]int {
+				"red": 0, 
+				"green": 0, 
+				"blue": 0,
+			}
 			// fmt.Println(set)
 			cubes := strings.Split(set, ", ")
 			for _, cube := range cubes {
-				idn := strings.Index(cube, " ")
-				cubecount, _ := strconv.Atoi(cube[:idn])
-				cubecolour := cube[idn + 1:]
+				idx := strings.Index(cube, " ")
+				cubecount, _ := strconv.Atoi(cube[:idx])
+				cubecolour := cube[idx + 1:]
 				gameDice[cubecolour] += cubecount
 				// fmt.Println("game", n+1, ":", gameDice)
 			}
+			if gameDice["red"] > bagDice["red"] || gameDice["green"] > bagDice["green"] || gameDice["blue"] > bagDice["blue"] {
+				gameSuccess = false
+			}
 		}
-		if gameDice["red"] <= diceBag["red"] && gameDice["green"] <= diceBag["green"] && gameDice["blue"] <= diceBag["blue"] {
+		if gameSuccess == true {
 			sum += n+1
 		}
 	}
 	fmt.Println(sum)
 }
 
+
+
+func partTwo(lines []string) {
+	sum := 0
+
+	for _, line := range lines {
+		
+		game := line[strings.Index(line, ":")+2:]
+		sets := strings.Split(string(game), "; ")
+		// fmt.Println(sets)
+		
+		minDice := map[string]int {
+			"red": 0, 
+			"green": 0, 
+			"blue": 0,
+		}
+		
+		for _, set := range sets {
+			gameDice := map[string]int {
+				"red": 0, 
+				"green": 0, 
+				"blue": 0,
+			}
+			// fmt.Println(set)
+			cubes := strings.Split(set, ", ")
+			for _, cube := range cubes {
+				idx := strings.Index(cube, " ")
+				cubecount, _ := strconv.Atoi(cube[:idx])
+				cubecolour := cube[idx + 1:]
+				gameDice[cubecolour] += cubecount
+				if gameDice[cubecolour] > minDice[cubecolour] {
+					minDice[cubecolour] = gameDice[cubecolour]
+				}
+			}
+		}
+		sum += minDice["red"] * minDice["green"] * minDice["blue"]
+	}
+	fmt.Println(sum)
+}
+
 func main() {
-	test1 := "test1.txt"
-	lines := rf.ReadFile(test1)
+	// test1 := "test1.txt"
+	// lines := rf.ReadFile(test1)
+	inputtxt := "input.txt"
+	lines := rf.ReadFile(inputtxt)
 	partOne(lines)
+	partTwo(lines)
 }
