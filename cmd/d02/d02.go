15fe64d9bfaa9bb4f4fce37cbd81cde14a23e4cc
Rez <probablyrez@gmail.com>
2024-04-28 11:12:54 +1000

    day2 part 1

--- cmd/d02/d02.go
+++ cmd/d02/d02.go
@@ -1,19 +1,46 @@
 package main
 
-import rf "readfile"
-
-type Dicebag struct {
-	blue  int
-	red   int
-	green int
-}
+import (
+	"fmt"
+	rf "readfile"
+	"strconv"
+	"strings"
+)
 
 func partOne(lines []string) {
-	dicebag := Dicebag{red: 12, green: 13, blue: 14}
-	sum := 0
-	for _, line := range lines {
-
+	diceBag := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
 	}
+	sum := 0
+
+	for n, line := range lines {
+		gameDice := map[string]int {
+			"red": 0, 
+			"green": 0, 
+			"blue": 0,
+		}
+		game := line[strings.Index(line, ":")+2:]
+		sets := strings.Split(string(game), "; ")
+
+		// fmt.Println(sets)
+		for _, set := range sets {
+			// fmt.Println(set)
+			cubes := strings.Split(set, ", ")
+			for _, cube := range cubes {
+				idn := strings.Index(cube, " ")
+				cubecount, _ := strconv.Atoi(cube[:idn])
+				cubecolour := cube[idn + 1:]
+				gameDice[cubecolour] += cubecount
+				// fmt.Println("game", n+1, ":", gameDice)
+			}
+		}
+		if gameDice["red"] <= diceBag["red"] && gameDice["green"] <= diceBag["green"] && gameDice["blue"] <= diceBag["blue"] {
+			sum += n+1
+		}
+	}
+	fmt.Println(sum)
 }
 
 func main() {
