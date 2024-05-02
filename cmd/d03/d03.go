2e344864a7b5aeb96c1140e47549176ece7274a4
Rez <probablyrez@gmail.com>
2024-05-02 13:18:05 +1000

    Day 3, part one working

--- cmd/d03/d03.go
+++ cmd/d03/d03.go
@@ -33,91 +33,60 @@ func partOne(lines []string) {
 
 	// Scan lines for symbols, ignore periods
 	for y, line := range lines {
+		num := 0
+		count := 0
 		for x := 0; x < cols; x++ {
-			// parse numbers and add to a slice with locations
-			if unicode.IsDigit(rune(line[x])) {
-				num := 0
-				var t []int
-				var pnum PartNumber
-				tmpx := x 
-				for i := x; i < cols; i++ {
-					n, err := strconv.Atoi(string(line[i]))
-					if err == nil {
-						t = append(t, n)
-						num = (num * 10) + n
-					}  else {
-						break
-					}
-					x = i
-				}
-				pnum.value = num
-				pnum.len = len(t) - 1
-				pnum.coords.x = tmpx
-				pnum.coords.y = y
-				pnums = append(pnums, pnum)
-				if (x + 1) < cols {
-					x += 1
+			fmt.Print(string(line[x]))
+			n, err := strconv.Atoi(string(line[x]))
+			if err == nil {
+				num = (num * 10) + n
+				count++
+
+				if x >= cols -1 || !unicode.IsDigit(rune(line[x+1])){
+					var pnum PartNumber
+					pnum.value = num
+					pnum.len = count - 1 
+					pnum.coords.x = x - count + 1
+					pnum.coords.y = y
+					pnums = append(pnums, pnum)
+					num = 0
+					count = 0
 				}
 			}
-					
-					
-			// ignore periods "."
+
 			if string(line[x]) == "." {
 				continue
 			}
-			// n, err := strconv.Atoi(string(line[x]))
-			// if err == nil {
-			// 	t := n
-			// 	for i := 1; x+i < cols; i++ {
-			// 		fmt.Println(t, y, x, ":", x+i)
-			// 		n, err = strconv.Atoi(string(line[x+i]))
-			// 		if err == nil {
-			// 			t = (t * 10) + n
-			// 		} else {
-			// 			var pnum PartNumber
-			// 			pnum.value = t
-			// 			pnum.coords.y = y
-			// 			pnum.coords.x = x
-			// 			pnum.len = i - 1
-			// 			pnums = append(pnums, pnum)
-			// 			x += i - 1
-			// 			break
-			// 		}
-			// 	}
-			// 	continue
-			// }
-
+			
+			if !unicode.IsDigit(rune(line[x])) {
 			var loc SymbolLocation
 			loc.value = string(line[x])
 			loc.coords.y = y
 			loc.coords.x = x
 			slocs = append(slocs, loc)
+			}
 		}
-	}
-
-	for _, sloc := range slocs {
-		_, err := strconv.Atoi(sloc.value)
-		if err == nil {
-			fmt.Println(sloc)
-		}
+		fmt.Println()
 	}
 
 	// walk slocs, identify which parts are adjacent
 	for i := 0; i < len(slocs); i++ {
 		for j := 0; j < len(pnums); j++ {
-			// y position validation: WORKING
-			if slocs[i].coords.y-pnums[j].coords.y >= -1 && slocs[i].coords.y-pnums[j].coords.y <= 1 {
-				// fmt.Println("values: ", slocs[i], pnums[j])
-				// fmt.Println(pnums[j], "left side:", slocs[i].coords.x - pnums[j].coords.x, "right side:", slocs[i].coords.x - (pnums[j].coords.x + pnums[j].len))
+			// First check Y coordinate to make sure it's in range
+			vert := slocs[i].coords.y-pnums[j].coords.y 
+			if vert >= -1 && vert <= 1 {
 				leftside := slocs[i].coords.x - pnums[j].coords.x
 				rightside := slocs[i].coords.x - (pnums[j].coords.x + pnums[j].len)
-				// fmt.Println(pnums[j], leftside, rightside)
+				// Then check left vs right, because this is in an array with numbers of arbirary
+				// length we check left and right adjacency separately
 				if leftside >= -1 && leftside <= 1 {
-					pnums[j].adjacent = true
 					fmt.Println("left,\t", pnums[j], ",\t", slocs[i], ",", leftside)
-				} else if rightside >= -1 && rightside <= 1 {
 					pnums[j].adjacent = true
+					sum += pnums[j].value
+				} else if rightside >= -1 && rightside <= 1 {
 					fmt.Println("right,\t", pnums[j], ",\t", slocs[i], ",", rightside)
+					pnums[j].adjacent = true
+					sum += pnums[j].value
 				}
 			}
 		}
