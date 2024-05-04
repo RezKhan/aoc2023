4b7de37325f664b198af3211481649645f00f5b9
Rez <probablyrez@gmail.com>
2024-05-05 07:42:33 +1000

    day 4, part two done

--- cmd/d04/d04.go
+++ cmd/d04/d04.go
@@ -22,33 +22,65 @@ func cardNumStrToInts(str string) []int {
 	return nums
 }
 
+func assessCardForWins(winningNums []int, heldNums[]int) (winValue int, winCount int) {
+	winValue = 0
+	winCount = 0
+	for _, held := range heldNums {
+		if slices.Index(winningNums, held) > -1 {
+			if winValue < 1 {
+				winValue = 1
+			} else {
+				winValue *= 2
+			}
+			winCount++
+		}
+	}
+	return winValue, winCount
+}
+
+
 func partOne(lines []string) {
 	sum := 0
 	for _, line := range lines {
-		winningNumsStr := line[strings.Index(line, ":")+2:strings.Index(line, "|") - 1]
-		heldNumsStr := line[strings.Index(line, "|") + 2:]
+		winningNumsStr := line[strings.Index(line, ":")+2 : strings.Index(line, "|")-1]
+		heldNumsStr := line[strings.Index(line, "|")+2:]
 		winningNums := cardNumStrToInts(winningNumsStr)
 		heldNums := cardNumStrToInts(heldNumsStr)
-		fmt.Println(heldNums, "|", winningNums)
-		
-		t := 0
-		for _, held := range heldNums {
-			if slices.Index(winningNums, held) > -1 {
-				if t < 1 {
-					t = 1
-				} else {
-					t *= 2
-				}
-			}
-		}
-		sum += t
-		fmt.Println(t, sum)
+		winValue, _ := assessCardForWins(winningNums, heldNums)
+
+		sum += winValue
 	}
 	fmt.Println(sum)
+}
+
+func partTwo(lines []string) {
+	var copies []int
+	for range lines {
+		copies = append(copies, 0)
+	}
+	for i, line := range lines {
+		winningNumsStr := line[strings.Index(line, ":")+2 : strings.Index(line, "|")-1]
+		heldNumsStr := line[strings.Index(line, "|")+2:]
+		winningNums := cardNumStrToInts(winningNumsStr)
+		heldNums := cardNumStrToInts(heldNumsStr)
+		for j := 0; j <= copies[i]; j++ {
+			_, winCount := assessCardForWins(winningNums, heldNums)
+			for n:= 1; n <= winCount; n++ {
+				copies[i+n]++
+			}
+		}
+	}
+	totalCards := 0
+	for i := range copies {
+		totalCards += 1 + copies[i]
+	}
+
+	fmt.Println(totalCards)
 }
 
 func main() {
 	// lines := rf.ReadFile("test.txt")
 	lines := rf.ReadFile("input.txt")
-	partOne(lines)
+	// partOne(lines)
+	partTwo(lines)
 }
